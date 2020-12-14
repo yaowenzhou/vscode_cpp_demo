64c6ca52e7475f339f9b72feab42dfdb6c32bd3a
yaowenzhou <yaowenzhou@foxmail.com>
2020-12-15 00:25:25 +0800

    vscode cpp project demo

---
+++ demo.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author       : yaowenzhou
+ * @Date         : 2020-12-12 11:26:45
+ * @LastEditors  : yaowenzhou
+ * @LastEditTime : 2020-12-12 15:20:19
+ * @version      : 0.1.0
+ * @Description  : vscode + cpp + mingw demo
+ */
+#include <iostream>
+
+using namespace std;
+/**
+ * @description: main() Entry function
+ * @param {*}
+ * @return {*}
+ */
+int main() {
+    cout << "Welcome to vscode c++ world!" << endl;
+    return 0;
+}
